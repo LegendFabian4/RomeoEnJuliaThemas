469b71ceffb12e658b10fe704fdf729cf1c4949c
LegendFabian4 <121681426+LegendFabian4@users.noreply.github.com>
2024-02-06 13:57:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function Roma() {
+    alert('Thema 1 is Romantiek, er komt veel liefde voor tussen Romeo en Julia');
+  }
+  document.getElementById('button1').addEventListener('click', Roma);
+  
+
+  function Trag() {
+    alert('Thema 2 is Tragedie, er is veel tragedie door bijvoorbeeld ruzie tussen de 2 families');
+  }
+  document.getElementById('button2').addEventListener('click', Trag);
+  
+function Dram() {
+    alert('Thema 3 is Drama, er is veel drama zoals emotionaliteit in dingen die er gebeuren');
+  }
+  document.getElementById('button3').addEventListener('click', Dram);
